cb04b2c3948f9be647410c916c0daa2dabef188c
Thiago Ferreira <thiagoferreira@NATURAL-012839.local>
2022-07-20 09:53:20 -0300

    Including new files

--- src/main/java/br/pb/thiagofb84jp/rest/OlaMundo.java
+++ src/main/java/br/pb/thiagofb84jp/rest/OlaMundo.java
@@ -3,12 +3,16 @@ package br.pb.thiagofb84jp.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
 
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-        System.out.println(response.getBody().asString());
-        System.out.println(response.statusCode());
+        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+        System.out.println(response.statusCode() == 200);
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
     }
 }
