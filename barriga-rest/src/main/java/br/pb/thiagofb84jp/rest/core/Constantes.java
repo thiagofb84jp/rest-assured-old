b1fbc94fc285aa7c412a5ddf3dc3904127bfffbd
Thiago Ferreira <thiagoferreira@NATURAL-012839.local>
2022-09-05 12:23:02 -0300

    Including new files related to the REST-Assured course

---
+++ barriga-rest/src/main/java/br/pb/thiagofb84jp/rest/core/Constantes.java
@@ -0,0 +1,5 @@
+package br.pb.thiagofb84jp.rest.core;
+
+public interface Constantes {
+
+}
--- src/main/java/br/pb/thiagofb84jp/rest/HTML.java
+++ src/main/java/br/pb/thiagofb84jp/rest/HTML.java
@@ -1,6 +1,8 @@
 package br.pb.thiagofb84jp.rest;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import org.codehaus.groovy.transform.SourceURIASTTransformation;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -41,5 +43,41 @@ public class HTML {
              .body(hasXPath("count(//table/tr)", is("4")))
              .body(hasXPath("//td[text() = '2']/../td[2]", is("Maria Joaquina")))
         ;
+    }
+
+    @Test
+    public void deveAcessarAplicacaoWeb() {
+        String cookie = given()
+                             .log().all()
+                             .formParam("email", "thiago.ferreira@gmail.com")
+                             .formParam("senha", "abcd_123")
+                             .contentType(ContentType.URLENC.withCharset("UTF-8"))
+                        .when()
+                             .post("http://seubarriga.wcaquino.me/logar")
+                        .then()
+                             .log().all()
+                             .statusCode(200)
+                             .extract().header("set-cookie")
+        ;
+
+        cookie = cookie.split("=")[1].split(";")[0];
+        System.out.println(cookie);
+
+        String body = given()
+                           .log().all()
+                           .cookie("connect.sid", cookie)
+                      .when()
+                           .get("http://seubarriga.wcaquino.me/contas")
+                      .then()
+                           .log().all()
+                           .statusCode(200)
+                      .and()
+                           .body("html.body.table.tbody.tr[0].td[0]", is("Conta 1020"))
+                           .extract().body().asString()
+        ;
+
+        System.out.println("|-----------------------|");
+        XmlPath xmlPath = new XmlPath(XmlPath.CompatibilityMode.HTML, body);
+        System.out.println(xmlPath.getString("html.body.table.tbody.tr[0].td[0]"));
     }
 }
