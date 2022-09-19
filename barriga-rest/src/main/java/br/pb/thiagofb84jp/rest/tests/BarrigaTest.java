242ab4876d51d98f5fa8003333d5237cae762c22
Thiago Ferreira <thiagoferreira@NATURAL-012839.local>
2022-09-19 19:04:07 -0300

    Including more tests about REST-Assured

--- barriga-rest/src/main/java/br/pb/thiagofb84jp/rest/tests/BarrigaTest.java
+++ barriga-rest/src/main/java/br/pb/thiagofb84jp/rest/tests/BarrigaTest.java
@@ -7,8 +7,12 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 
 import static io.restassured.RestAssured.*;
@@ -72,4 +76,121 @@ public class BarrigaTest extends BaseTest {
              .body("nome", is("Conta alterada"))
         ;
     }
+
+    @Test
+    public void naoDeveInserirContaMesmoNome() {
+        given()
+             .header("Authorization", "JWT " + TOKEN)
+             .body("{\"nome\": \"Conta alterada\"}")
+        .when()
+             .post("/contas")
+        .then()
+             .statusCode(400)
+        .and()
+             .body("error", is("Já existe uma conta com esse nome!"))
+        ;
+    }
+
+    @Test
+    public void deveInserirMovimentacaoComSucesso() {
+        Movimentacao mov = getMovimentacaoValida();
+
+        given()
+             .header("Authorization", "JWT " + TOKEN)
+             .body(mov)
+        .when()
+             .post("/transacoes")
+        .then()
+             .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void deveValidarCamposObrigatoriosMovimentacao() {
+        given()
+             .header("Authorization", "JWT " + TOKEN)
+             .body("{}")
+        .when()
+             .post("/transacoes")
+        .then()
+             .statusCode(400)
+        .and()
+             .body("$", hasSize(8))
+             .body("msg", hasItems(
+                     "Data da Movimentação é obrigatório",
+                     "Data do pagamento é obrigatório",
+                     "Descrição é obrigatório",
+                     "Interessado é obrigatório",
+                     "Valor é obrigatório",
+                     "Valor deve ser um número",
+                     "Conta é obrigatório",
+                     "Situação é obrigatório"
+             ))
+        ;
+    }
+
+    @Test
+    public void deveInserirMovimentacaoComDataFutura() {
+        Movimentacao mov = getMovimentacaoValida();
+        mov.setData_transacao(getFutureDate());
+
+        given()
+             .header("Authorization", "JWT " + TOKEN)
+             .body(mov)
+        .when()
+             .post("/transacoes")
+        .then()
+             .statusCode(400)
+        .and()
+             .body("$", hasSize(1))
+             .body("msg", hasItem("Data da Movimentação deve ser menor ou igual à data atual"))
+        ;
+    }
+
+    /*
+    *   Generating mass of data
+     */
+    private Movimentacao getMovimentacaoValida() {
+        Movimentacao mov = new Movimentacao();
+
+        mov.setConta_id(1381328);
+//        mov.setUsuarioId();
+        mov.setDescricao("Descrição da movimentação");
+        mov.setEnvolvido("Envolvido na mov");
+        mov.setTipo("REC");
+        mov.setData_transacao(getPreviousDate());
+        mov.setData_pagamento(getCurrentDate());
+        mov.setValor(100F);
+        mov.setStatus(true);
+
+        return mov;
+    }
+
+    private String getCurrentDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String currentDate = dateFormat.format(new Date());
+
+        return currentDate;
+    }
+
+    private String getPreviousDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -1);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String futureDate = dateFormat.format(new Date((calendar.getTimeInMillis())));
+
+        return futureDate;
+    }
+
+    private String getFutureDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, +1);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String futureDate = dateFormat.format(new Date((calendar.getTimeInMillis())));
+
+        return futureDate;
+    }
+
 }
