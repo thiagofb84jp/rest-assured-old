086b17057430ee545b1d060ccf6f17aee2b156ec
Thiago Ferreira <thiagoferreira@NATURAL-012839.local>
2022-10-18 01:29:10 -0300

    Including more exercises

--- exercises-rest/src/main/java/br/pb/thiagofb84jp/rest/dummyJson/dto/Product.java
+++ exercises-rest/src/main/java/br/pb/thiagofb84jp/rest/dummyJson/dto/Product.java
@@ -1,8 +1,18 @@
 package br.pb.thiagofb84jp.rest.dummyJson.dto;
 
+import java.util.ArrayList;
+
 public class Product {
 
     private String title;
+    private String description;
+    private Float price;
+    private String category;
+    private Float rating;
+    private Integer stock;
+    private String brand;
+    private String thumbnail;
+    private ArrayList<String> images;
 
     public String getTitle() {
         return title;
@@ -10,5 +20,69 @@ public class Product {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public ArrayList<String> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<String> images) {
+        this.images = images;
     }
 }
--- exercises-rest/src/main/java/br/pb/thiagofb84jp/rest/dummyJson/tests/ProductsTest.java
+++ exercises-rest/src/main/java/br/pb/thiagofb84jp/rest/dummyJson/tests/ProductsTest.java
@@ -6,13 +6,15 @@ import com.github.javafaker.Faker;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class ProductsTest extends BaseTest {
 
     @Test
-    public void createNewProduct() {
+    public void createProduct() {
         Product product = getValidProduct();
 
         given()
@@ -24,6 +26,14 @@ public class ProductsTest extends BaseTest {
         .and()
              .body("id", is(notNullValue()))
              .body("title", is(product.getTitle()))
+             .body("description", is(product.getDescription()))
+             .body("price", is(product.getPrice()))
+             .body("rating", is(product.getRating()))
+             .body("stock", is(product.getStock()))
+             .body("brand", is(product.getBrand()))
+             .body("category", is(product.getCategory()))
+             .body("thumbnail", is(product.getThumbnail()))
+             .body("images", is(product.getImages()))
         ;
     }
 
@@ -41,19 +51,69 @@ public class ProductsTest extends BaseTest {
         .and()
              .body("id", is("1"))
              .body("title", is(product.getTitle()))
-             .body("images", hasSize(5))
-             .body("images", hasItems("https://dummyjson.com/image/i/products/1/1.jpg",
-                                         "https://dummyjson.com/image/i/products/1/2.jpg",
-                                         "https://dummyjson.com/image/i/products/1/3.jpg",
-                                         "https://dummyjson.com/image/i/products/1/4.jpg",
-                                         "https://dummyjson.com/image/i/products/1/thumbnail.jpg"))
+             .body("description", is(product.getDescription()))
+             .body("price", is(product.getPrice()))
+             .body("rating", is(product.getRating()))
+             .body("stock", is(product.getStock()))
+             .body("brand", is(product.getBrand()))
+             .body("category", is(product.getCategory()))
+             .body("thumbnail", is(product.getThumbnail()))
+             .body("images", is(product.getImages()))
+        ;
+    }
+
+    @Test
+    public void getSingleProduct() {
+        given()
+             .pathParam("id", 38)
+        .when()
+             .get("/products/{id}")
+        .then()
+             .statusCode(200)
+        .and()
+             .body("id", is(38))
+             .body("title", is("sublimation plain kids tank"))
+             .body("description", is("sublimation plain kids tank tops wholesale"))
+             .body("price", is(100))
+             .body("discountPercentage", is(11.12F))
+             .body("rating", is(4.8F))
+             .body("stock", is(20))
+             .body("brand", is("Soft Cotton"))
+             .body("category", is("tops"))
+             .body("thumbnail", is("https://dummyjson.com/image/i/products/38/thumbnail.jpg"))
+             .body("images", hasItems("https://dummyjson.com/image/i/products/38/1.png",
+                                         "https://dummyjson.com/image/i/products/38/2.jpg",
+                                         "https://dummyjson.com/image/i/products/38/3.jpg",
+                                         "https://dummyjson.com/image/i/products/38/4.jpg"))
+             .body("images", hasSize(4))
         ;
     }
 
     private static Product getValidProduct() {
-        Faker faker = new Faker();
         Product product = new Product();
-        product.setTitle(faker.commerce().productName());
+        product.setTitle("iPhone 14 XMas Pro Ultimate Generation");
+        product.setDescription("This is the new iPhone 14 XMas Pro Ultimate Generation");
+        product.setPrice(4009.99F);
+        product.setRating(4.11F);
+        product.setStock(100);
+        product.setBrand("YIOSI");
+        product.setCategory("smartphones");
+        product.setThumbnail("https://dummyjson.com/image/i/products/100/thumbnail.jpg");
+
+        ArrayList<String> images = new ArrayList<String>();
+        images.add("https://dummyjson.com/image/i/products/100/1.jpg");
+        images.add("https://dummyjson.com/image/i/products/100/2.jpg");
+        images.add("https://dummyjson.com/image/i/products/100/3.jpg");
+        images.add("https://dummyjson.com/image/i/products/100/thumbnail.jpg.jpg");
+        product.setImages(images);
+
+        return product;
+    }
+
+    private static Product updateValidProduct() {
+        Product product = new Product();
+        product.setTitle("iPhone 14 XMas Pro Ultimate Generation");
+
         return product;
     }
 
